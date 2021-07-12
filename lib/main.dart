75aca818c543135dfa19ef7d186b023604deacbe
wilcywilson <wilsonshresthaapps@gmail.com>
2021-07-12 19:13:49 +0545

    Added File Manager Pop Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,62 @@
-import 'package:encryptionapp/presentation/my_flutter_app_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:encryptionapp/presentation/my_flutter_app_icons.dart';
+import 'package:flutter/services.dart';
 import 'build_encrypt_button.dart';
 import 'build_decrypt_button.dart';
 import 'enter_password.dart';
 
 void main() => runApp(NinjaCard()); // Moved Scaffold to class NinjaCard
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String? _fileName;
+  List<PlatformFile>? _paths;
+  String? _directoryPath;
+  String? _extension;
+  bool _loadingPath = false;
+  bool _multiPick = false;
+  bool _hasValidMime = false;
+  FileType _pickingType = FileType.any;
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() => _extension = _controller.text);
+  }
+
+  void _openFileExplorer() async {
+    setState(() => _loadingPath = true);
+    try {
+      _directoryPath = null;
+      _paths = (await FilePicker.platform.pickFiles(
+        type: _pickingType,
+        allowMultiple: _multiPick,
+        allowedExtensions: (_extension?.isNotEmpty ?? false)
+            ? _extension?.replaceAll(' ', '').split(',')
+            : null,
+      ))
+          ?.files;
+    } on PlatformException catch (e) {
+      print("Unsupported operation" + e.toString());
+    } catch (ex) {
+      print(ex);
+    }
+    if (!mounted) return;
+    setState(() {
+      _loadingPath = false;
+      print(_paths!.first.extension);
+      _fileName =
+          _paths != null ? _paths!.map((e) => e.name).toString() : '...';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,14 +78,14 @@ class NinjaCard extends StatelessWidget {
               child: Column(children: [
                 EnterPassword(),
                 BuildEncryptButton(), // Moved to another class for each
-                BuildDecryptButton(),               
+                BuildDecryptButton(),
               ]),
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton.extended(
           // Moved FloatingActionButton to bottom right
-          onPressed: () {},
+          onPressed: () => _openFileExplorer(),
           label: const Text('Add File'), // Added Custom Text
           icon: const Icon(MyFlutterApp.fileexplorericon), // Added Custom Icon
           backgroundColor: Colors.pink,
